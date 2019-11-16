f0a22e7bcc364d64fecafeddedd88eeb2310c6e8
Felons <slavikzhyhota@gmail.com>
2019-11-16 16:13:14 +0100

    Update secret.js

--- pages/secret.js
+++ pages/secret.js
@@ -1,16 +1,12 @@
 import SecureTemplate from '../public/secure-template';
 import React from 'react'
-import PropTypes from "prop-types";
 
-function Secret({loggedInUser}){
+function Secret(){
     return(
         <div>
             This is the secret page ! You can only see it when you are logged in !
         </div>
     );
 }
-Secret.propTypes = {
-    loggedInUser: PropTypes.object
-};
 
 export default SecureTemplate(Secret)
